bd12aea9c5a5db0fa8c629ff093922a663f8f0f9
malachi frazier <mjf22802@yahoo.com>
2023-11-08 20:02:57 -0800

    add LMEncoder class

---
+++ model/LMEncoder.py
@@ -0,0 +1,25 @@
+from sentence_transformers import SentenceTransformer
+
+class LMEncoder():
+    def __init__(self, model_name):
+
+        # Initialize encoder from HuggingFace sentence-transformers
+        self.model = SentenceTransformer(model_name)
+
+    def encode(self, sentences: Union[str, List[str]],
+               batch_size: int = 32,
+               output_value: str = 'sentence_embedding',
+               convert_to_numpy: bool = True,
+               convert_to_tensor: bool = False,
+               normalize_embeddings: bool = False) -> Union[List[Tensor], ndarray, Tensor]:
+        
+        output = self.model.encode(
+            sentences= sentences,
+            batch_size= batch_size,
+            output_value= output_value,
+            convert_to_numpy= convert_to_numpy,
+            convert_to_tensor= convert_to_tensor,
+            normalize_embeddings= normalize_embeddings
+        )
+
+        return output
